c6fd85d935e3213c5bd10ddd4cefd6b33fe14ad3
Dimmi <pechunka11@gmail.com>
2022-03-30 15:46:10 +0200

    sdf

--- pages/api/money/exportImg.js
+++ pages/api/money/exportImg.js
@@ -3,19 +3,8 @@ import fs from 'fs'
 import FormData from 'form-data'
 import slugify from 'slugify'
 import axios from 'axios';
+import {loadImage} from 'canvas'
 import { Blob } from 'buffer';
-
-function DataURIToBlob(dataURI) {
-  const splitDataURI = dataURI.split(',')
-  const byteString = splitDataURI[0].indexOf('base64') >= 0 ? atob(splitDataURI[1]) : decodeURI(splitDataURI[1])
-  const mimeString = splitDataURI[0].split(':')[1].split(';')[0]
-
-  const ia = new Uint8Array(byteString.length)
-  for (let i = 0; i < byteString.length; i++)
-      ia[i] = byteString.charCodeAt(i)
-
-  return new Blob([ia], { type: mimeString })
-}
 
 export default async function handler (req, res) {
   if(req.method == 'POST') {
@@ -27,27 +16,30 @@ export default async function handler (req, res) {
 
     const length = products.length
 
-    
-
-    for(var i = 0; i < 20; i++) {
+    for(var i = 10; i < 13; i++) {
       resStrapi = await AxiosSTRAPI.get(`/produkties?product_id=${products[i].product_id}&_publicationState=preview`)
       if(resStrapi.data.length && resStrapi.data[0].imageName?.length) {
         
-        
-        const file = fs.readFileSync('public/catalog/'+resStrapi.data[0].imageName)
-        // let name = resStrapi.data[0].imageName.split('.')
-        const base64 = file.toString('base64')
-        const fileBlob = DataURIToBlob(base64)
-        console.log(fileBlob);
-        // let blobFile = new Blob([file], {type: name[name.length - 1]})
-        // let formData = new FormData()
-        // formData.append('files', blobFile)
-        // formData.append('ref', 'produkties')
-        // formData.append('refId', resStrapi.data[0].id)
-        // formData.append('field', 'images')
-        // console.log(formData);
-        // AxiosSTRAPI.post('/upload', formData).then(res => console.log(res.data))
-        //     .catch(err => console.log('Not uploud --', err.response.data.data.errors))
+        const file = fs.readFileSync('data/catalog/'+resStrapi.data[0].imageName)
+
+        let base = file.toString('base64')
+
+        let dataSend = {
+          files: base,
+          ref: 'produkties',
+          refId: `${resStrapi.data[0].id}`,
+          field: 'images'
+        }
+
+        // console.log(dataSend)
+
+        let formData = new FormData()
+        formData.append('file', dataSend.files)
+        formData.append('ref', dataSend.ref)
+        formData.append('refId', dataSend.refId)
+        formData.append('field', dataSend.field)
+        AxiosSTRAPI.post('/upload', formData).then(res => console.log(res.data))
+          .catch(err => console.log('Not uploud --', err.response.data))
 
       }else{
         console.log(i, '/', length)
