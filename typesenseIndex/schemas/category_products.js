5ee39c36288905ef8eebf3fdad6a582a6dd9f40a
Your Name <>
2022-05-30 21:14:50 +0200

    add new index

---
+++ typesenseIndex/schemas/category_products.js
@@ -0,0 +1,53 @@
+module.exports = {
+  name: "category_products",
+  num_documents: 0,
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      facet: false,
+    },
+    {
+      name: "slug",
+      type: "string",
+      facet: false,
+    },
+    {
+      name: "image",
+      type: "string",
+      facet: false,
+      optional: true,
+    },
+    {
+      name: "price",
+      type: "int32",
+      facet: true,
+    },
+    {
+      name: "brand",
+      type: "string",
+      facet: true
+    },
+    {
+      name: "brandsSlug",
+      type: "string",
+      facet: true
+    },
+    {
+      name: "category",
+      type: "string[]",
+      facet: true
+    },
+    {
+      name: "categorySlug",
+      type: "string[]",
+      facet: false,
+    },
+    {
+      name: "values",
+      type: "string[]",
+      facet: true,
+    },
+    
+  ]
+}
