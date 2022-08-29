41a1b5b01928c2c705d25e6e4cb40113f8d91e18
Your Name <>
2022-08-29 14:44:39 +0200

    dfsgfg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,14 +6,10 @@ module.exports = {
       GP_CLIENT_ID: "1693740873",
       GP_GO_ID: "8169154323",
       GP_SECRET_ID: "933V3mFV",
-      APP_API: "https://strapi.kralovska-pece.cz",
-      APP_DOMAIN: "https://kralovska-pece.cz",
+      APP_API: "http://kp-strapi-demo.hardart.cz",
+      APP_DOMAIN: "http://kp-demo.hardart.cz",
       PAY_API: "https://gate.gopay.cz",
       SENDGRID_API_KEY: "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",
-      TYPESENSE_SEARCH_ONLY_API_KEY: "sdfgsdfgsdfgfsdg",
-      TYPESENSE_HOST: "kp-search.hardart.cz",
-      TYPESENSE_PORT: "443",
-      TYPESENSE_PROTOCOL: "https"
     }
   }],
 
@@ -23,8 +19,8 @@ module.exports = {
       host : ['89.221.216.23'],
       ref  : 'origin/main',
       repo : 'git@github.com:simon1400/kralovska-pece.git',
-      path : '/var/www/kralovska-pece/client',
-      'post-deploy' : 'yarn && yarn build && yarn populate && pm2 reload ecosystem.config.js --env production',
+      path : '/var/www/kralovska-pece/client-demo',
+      'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
     }
   }
 };
