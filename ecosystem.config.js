9399f47069ae2b0c6708ea4adcf64f9352567469
Your Name <>
2023-01-04 10:37:12 +0100

    asfgasdf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       user : 'dimi',
       host : ['89.221.216.23'],
       ref  : 'origin/main',
-      repo : 'git@github.com:simon1400/kp-demo.git',
+      repo : 'git@github.com:simon1400/kp-client.git',
       path : '/var/www/kralovska-pece/client-demo',
       'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
     }
