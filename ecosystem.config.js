c1dfcc3bc7d0bc1562ee5f99628a266097c99989
Your Name <>
2023-01-04 10:38:23 +0100

    fixed env file and hide variables

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,7 @@
 module.exports = {
   apps : [{
     name   : "KP demo client",
-    script : "yarn start",
-    env_production: {
-      GP_CLIENT_ID: "1693740873",
-      GP_GO_ID: "8169154323",
-      GP_SECRET_ID: "933V3mFV",
-      APP_API: "https://strapi.kralovska-pece.cz",
-      APP_DOMAIN: "https://kralovska-pece.cz",
-      PAY_API: "https://gate.gopay.cz",
-      SENDGRID_API_KEY: "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",
-    }
+    script : "yarn start"
   }],
 
   deploy : {
